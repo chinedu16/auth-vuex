307347c0ca029906c072ce5dab40237cbea9279c
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-06-13 16:02:40 +0100

    authentication

--- src/main.js
+++ src/main.js
@@ -2,16 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
-
-Vue.prototype.$http = axios
+import Axios from 'axios';
 
 Vue.config.productionTip = false
 
-// const token = localStorage.getItem('token');
-// if (token) {
-//   Vue.prototype.$http.defaults.common['Authorization'] = token;
-// }
+const token = localStorage.getItem('token')
+if (token) {
+  Axios.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   router,
