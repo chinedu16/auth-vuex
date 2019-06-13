46511052500266b82c44a92e251d1dde92e82198
Chinedu Ohagwu <chinedu.ohagwu@gmail.com>
2019-06-13 13:03:47 +0100

    Add axios to project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+// const token = localStorage.getItem('token');
+// if (token) {
+//   Vue.prototype.$http.defaults.common['Authorization'] = token;
+// }
 
 new Vue({
   router,
